56621a08178ecc50f10d43e415f26f7a4cc4cd3d
ShivamRauniyaar <145185374+ShivamRauniyaar@users.noreply.github.com>
2025-03-02 23:39:49 +0530

    some commited

--- play.js
+++ play.js
@@ -40,3 +40,16 @@
 // const copiedArray = [...hobbies]
 // console.log(copiedArray)
 
+
+//patterns 
+
+
+for(let i = 0;i<=5;i++){
+    let str = "";
+    for(let j =1;j<=i;j++){
+
+        str += "*"
+    }
+    console.log(str)
+}
+
